fb5e44c1f9f83056db35a6f449dfe81236762344
tenshi <zakan.6000@gmail.com>
2024-10-13 13:51:55 +0800

    add task 7 FGPredel

--- lin_alg_with_if_while/lin_alg_with_if_while.cpp
+++ lin_alg_with_if_while/lin_alg_with_if_while.cpp
@@ -1,28 +1,46 @@
-﻿#include <iostream>
+﻿#define _USE_MATH_DEFINES
+
+#include <iostream>
 #include <iomanip>
 #include <cmath>
 
 using namespace std;
 
+int tFirstFx();
+int tSecondFreq();
+void tThirdEllipse();
 int tForthCommerce();
 int tFifthMilesInKilometers();
-void tFirstFx();
+void tSixthBinNewton();
+void tSeventhFGPredel();
 
 int main(void)
 {
-    tFirstFx();
+    tSeventhFGPredel();
     return 0;
 }
+
 float f4(float x)
 {
-   return x * x * x * x;
+    return x * x * x * x;
 }
+
 float fTan(float x)
 {
     return tan(x);
 }
 
-void tFirstFx(void)
+float fExp(float x)
+{
+    return pow(M_E, x);
+}
+
+float fSin2(float x)
+{
+    return sin(x) * sin(x);
+}
+
+int tFirstFx(void)
 {
     int parity = 0, odd = 0, general = 0;
 
@@ -32,31 +50,74 @@ void tFirstFx(void)
             continue;
         }
 
-        float f = fTan(x);
-        float f_minus_x = fTan(-x);
-        cout << f << ' ' << f_minus_x << ' ' << f << endl;
+        float f = fExp(x);
+        float f_minus_x = fExp(-x);
 
         if (f == f_minus_x)
         {
             parity++;
         }
-        else if (f == -f)
+        else if (f_minus_x == -f)
         {
             odd++;
         }
         else
         {
             cout << "Function is general";
-            break;
+            return 1;
         }
     }
-    if (parity > 0 && odd == 0 && general == 0)
+    if (parity > 0 && odd == 0)
     {
         cout << "Function is parity";
     }
-    else if (odd > 0 && parity == 0 && general == 0)
+    else if (odd > 0 && parity == 0)
     {
-        cout << "Funcrion is odd";
+        cout << "Function is odd";
+    }
+    return 1;
+}
+
+int tSecondFreq(void)
+{
+    float freq = M_PI;
+
+    for (float x = 0; x <= 5 * freq; x += 0.1)
+    {
+        float f = fSin2(x);
+        float f_freq = fSin2(x + freq);
+        cout << f << ' ' << f_freq;
+        if (f != f_freq)
+        {
+            cout << "quit";
+            return 0;
+        }
+    }
+    cout << "f(x) is frequently with frequency = " << freq;
+    return 1;
+}
+
+void tThirdEllipse(void)
+{
+    float a, b;
+    cout << "Enter the a, b parametrs (a b): ";
+    cin >> a >> b;
+
+    for (int y = -(int)b; y <= b; y++)
+    {
+        for (int x = (int)a; x >= -a; x--)
+        {
+           float ellipse = x * x / a / a + y * y / b / b;
+           if (ellipse <= 1)
+           {
+               cout << '*';
+           }
+           else
+           {
+               cout << ' ';
+           }
+        }
+        cout << endl;
     }
 }
 
@@ -91,7 +152,6 @@ int tFifthMilesInKilometers(void)
 
     miles = 0;
     kilometers = 0;
-    cout << "Введите что-то там";
     cout << "Enter the limit to which you want to build the table (km): ";
     cin >> k;
     cout << "Miles    Kilometrs" << endl;
@@ -132,4 +192,47 @@ int tFifthMilesInKilometers(void)
         }
     }
     return 1;
+}
+
+float numberComb(float m, float i)
+{
+    if (i == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return numberComb(m, i - 1) * (m - i + 1) / i;
+    }
+}
+
+void tSixthBinNewton(void)
+{
+    float x, m;
+    float bin = 0;
+    cout << "Enter the x, m values for (1+x)^m (x m): ";
+    cin >> x >> m;
+    
+    cout << pow(1 + x, m) << endl;
+
+    for (int i = 0; i <= m; i++)
+    {
+        bin += numberComb(m, i) * pow(x, i);
+    }
+    cout << bin << endl;
+}
+
+void tSeventhFGPredel(void)
+{
+    double e = 0.000001;
+    //cout << "Enter the error rate (e): ";
+    //cin >> e;
+    double FGPredel;
+    double x = 1;
+    do
+    {
+        FGPredel = sin(x) / x;
+        cout << "Predel:" << FGPredel << " x: " << x << ' ' << 1 - FGPredel << endl;
+        x /= 2;
+    } while (fabs(1 - FGPredel) >= e);
 }
