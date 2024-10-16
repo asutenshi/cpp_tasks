d94023aae16492342e004eeeb0f6c2cb950dde4b
TenshiLenovo <zakan.6000@gmail.com>
2024-10-16 17:20:51 +0800

    add 8th task SGPredel, work on 2nd and 9th tasks

--- lin_alg_with_if_while/lin_alg_with_if_while.cpp
+++ lin_alg_with_if_while/lin_alg_with_if_while.cpp
@@ -7,16 +7,18 @@
 using namespace std;
 
 int tFirstFx();
-int tSecondFreq();
+//int tSecondFreq();
 void tThirdEllipse();
 int tForthCommerce();
 int tFifthMilesInKilometers();
 void tSixthBinNewton();
 void tSeventhFGPredel();
+void tEighthSGPredel();
 
 int main(void)
 {
     tSeventhFGPredel();
+    //tEighthSGPredel();
     return 0;
 }
 
@@ -42,60 +44,106 @@ float fSin2(float x)
 
 int tFirstFx(void)
 {
-    int parity = 0, odd = 0, general = 0;
+    double a_start = -5.0;
+    double a_end = 5.0;
+    double a_step = 0.1;
 
-    for (float x = -5; x <= 5; x += 0.1)
-    {
-        if (x == 0) {
+    double eps = 1.0E-6;
+
+//---------------------------------------------------------------//
+
+    bool flag = 1;
+    for (double x = a_start; x <= a_end; x += a_step) {
+        if (x == 0)
             continue;
-        }
+        if (fabs(fTan(x) - fTan(-x)) > eps) {
+            flag = 0;
+            break;
+       }
+    }
+    if (flag == 1) {
+        cout << "Function is paryty" << endl;
+        return 1;
+    }
 
-        float f = fExp(x);
-        float f_minus_x = fExp(-x);
+//---------------------------------------------------------------//
 
-        if (f == f_minus_x)
-        {
-            parity++;
-        }
-        else if (f_minus_x == -f)
-        {
-            odd++;
-        }
-        else
-        {
-            cout << "Function is general";
-            return 1;
-        }
+    flag = 1;
+    for (double x = a_start; x <= a_end; x += a_step) {
+        if (x == 0)
+            continue;
+        if (fabs(fTan(x) + fTan(-x)) > eps) {
+            flag = 0;
+            break;
+       }
     }
-    if (parity > 0 && odd == 0)
-    {
-        cout << "Function is parity";
+    if (flag == 1) {
+        cout << "Function is odd" << endl;
+        return 1;
     }
-    else if (odd > 0 && parity == 0)
-    {
-        cout << "Function is odd";
+    else {
+        cout << "Function is general" << endl;
+        return 1;
     }
-    return 1;
+
+//---------------------------------------------------------------//
+
+    // Решение до исправлений:
+
+    //int parity = 0, odd = 0, general = 0;
+
+    //for (float x = -5; x <= 5; x += 0.1)
+    //{
+    //    if (x == 0) {
+    //        continue;
+    //    }
+
+    //    float f = fTan(x);
+    //    float f_minus_x = fTan(-x);
+
+    //    if (f == f_minus_x && odd == 0)
+    //    {
+    //        parity++;
+    //    }
+    //    else if (f_minus_x == -f_minux_x && parity == 0)
+    //    {
+    //        odd++;
+    //    }
+    //    else
+    //    {
+    //        cout << "Function is general";
+    //        return 1;
+    //    }
+    //}
+    //if (parity > 0)
+    //{
+    //    cout << "Function is parity";
+    //}
+    //else if (odd > 0)
+    //{
+    //    cout << "Function is odd";
+    //}
+    //return 1;
 }
 
-int tSecondFreq(void)
-{
-    float freq = M_PI;
-
-    for (float x = 0; x <= 5 * freq; x += 0.1)
-    {
-        float f = fSin2(x);
-        float f_freq = fSin2(x + freq);
-        cout << f << ' ' << f_freq;
-        if (f != f_freq)
-        {
-            cout << "quit";
-            return 0;
-        }
-    }
-    cout << "f(x) is frequently with frequency = " << freq;
-    return 1;
-}
+//int tSecondFreq(void)
+//{
+//    float freq = M_PI;
+//
+//    for (float x = 0; x <= 5 * freq; x += 0.1)
+//    {
+//        float f = fSin2(x);
+//        float f_freq = fSin2(x + freq);
+//        cout << f << ' ' << f_freq;
+//        if (f != f_freq)
+//        {
+//            cout << "quit";
+//            return 0;
+//        }
+//    }
+//    cout << "f(x) is frequently with frequency = " << freq;
+//    return 1;
+//}
 
 void tThirdEllipse(void)
 {
@@ -103,9 +151,13 @@ void tThirdEllipse(void)
     cout << "Enter the a, b parametrs (a b): ";
     cin >> a >> b;
 
-    for (int y = -(int)b; y <= b; y++)
+//---------------------------------------------------------------//
+
+    // буду проверять принадлежность x (от -a до a) и y (от b до -b) к эллипсу
+
+    for (int y = (int)b; y >= -b; y--)
     {
-        for (int x = (int)a; x >= -a; x--)
+        for (int x = -(int)a; x <= a; x++)
         {
            float ellipse = x * x / a / a + y * y / b / b;
            if (ellipse <= 1)
@@ -130,6 +182,9 @@ int tForthCommerce(void)
     cin >> k >> p;
     cout << "Enter the profit per year (value): ";
     cin >> r;
+
+//---------------------------------------------------------------//
+
     if (r > k * p / 100) {
         while (k > 0)
         {
@@ -148,48 +203,49 @@ int tForthCommerce(void)
 
 int tFifthMilesInKilometers(void)
 {
-    float miles, kilometers, k;
+    float k;
+    double miles = 0;
+    double kilometers = 0;
 
-    miles = 0;
-    kilometers = 0;
     cout << "Enter the limit to which you want to build the table (km): ";
     cin >> k;
     cout << "Miles    Kilometrs" << endl;
     
+//---------------------------------------------------------------//
+
     while (k > kilometers)
     {
         kilometers += 1;
         miles += 1;
         if (k > kilometers)
         {
-        cout << fixed << setprecision(4) << left << setw(9) << kilometers * 0.6214 << kilometers << endl;
+            cout << fixed << setprecision(4) << left << setw(9) << kilometers * 0.6214 << kilometers << endl;
         }
+        else break;
         if (k > miles * 1.609344)
-        cout << fixed << setprecision(4) << left << setw(9) << miles << miles * 1.609344 << endl;
+        {
+            cout << fixed << setprecision(4) << left << setw(9) << miles << miles * 1.609344 << endl;
+        }
+        else break;
 
         kilometers += 1;
         if (k > kilometers)
         {
         cout << fixed<< setprecision(4) << left << setw(9) << kilometers * 0.6214 << kilometers << endl;
         }
-        else
-        {
-            return 1;
-        }
+        else break;
         kilometers += 1;
         if (k > kilometers)
         {
         cout << fixed << setprecision(4) << left << setw(9) << kilometers * 0.6214 << kilometers << endl;
         }
-        else
-        {
-            return 1;
-        }
+        else break;
         miles += 1;
         if (k > miles * 1.609344)
         {
-        cout << fixed<< setprecision(4) << left << setw(9) << miles << miles * 1.609344 << endl;
+            cout << fixed << setprecision(4) << left << setw(9) << miles << miles * 1.609344 << endl;
         }
+        else break;
     }
     return 1;
 }
@@ -224,15 +280,50 @@ void tSixthBinNewton(void)
 
 void tSeventhFGPredel(void)
 {
-    double e = 0.000001;
+    double e = 0.000000000001;
     //cout << "Enter the error rate (e): ";
     //cin >> e;
     double FGPredel;
     double x = 1;
+    int i = 0;
     do
     {
         FGPredel = sin(x) / x;
-        cout << "Predel:" << FGPredel << " x: " << x << ' ' << 1 - FGPredel << endl;
+        cout << "Predel " << setw(3) << setfill('0') << right << i << " : " << setw(10) << setfill('0') << setprecision(6) << FGPredel << " | ";
+        cout << "x: " << left << setw(12) << setfill('0') << setprecision(6) << x << " | ";
+        cout << "Equtation: " << setw(11) << setfill('0') << setprecision(6) << 1 - FGPredel << endl;
         x /= 2;
+        i++;
     } while (fabs(1 - FGPredel) >= e);
+}
+void tEighthSGPredel(void)
+{
+    double e = 0.0001;
+    //cout << "Enter the error rate (e): ";
+    //cin >> e;
+    double SGPredel;
+    double n = 1;
+    do
+    {
+        SGPredel = pow(1 + 1 / n, n);
+        cout << "n: " << right << setw(5) << setfill('0') << n << " | ";
+        cout << "Predel : " << left << setw(10) << setfill('0') << setprecision(6) << SGPredel << " | ";
+        cout << "Equtation: " << setw(11) << setfill('0') << setprecision(6) << fabs(M_E - SGPredel) << endl;
+        n++;
+    } while (fabs(M_E - SGPredel) >= e);
+}
+
+void tNinethInfFraction(void)
+{
+    int frac = 2;
+    int e = 100;
+    for (int i = 2; i <= e; i++)
+    {
+        frac += 1 / tgamma(i);
+    }
+    
+    for (int i = e; i > 0; i--)
+    {
+
+    }
 }
