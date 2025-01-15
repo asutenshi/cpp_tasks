2e867fce75fa526bee6de0ab99e90e8be3ddc11c
TenshiLenovo <zakan.6000@gmail.com>
2025-01-15 13:19:41 +0800

    work on stek

--- ukazateli/stek.cpp
+++ ukazateli/stek.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+int brackets(const char c);
+
 int main(void) {
   int const SIZE_STR = 80;
   char str[SIZE_STR + 1] = {'\0'};
@@ -13,7 +15,53 @@ int main(void) {
 
   cin.getline(str, SIZE_STR);
 
-  cout << SIZE_STEK;
+  while (*pstr != '\0') {
+    int bracket = brackets(*pstr);
+    if (bracket > 0) {
+      *pstek = *pstr;
+      pstek++;
+    } else if (bracket < 0) {
+      if (brackets(*pstek) == -bracket) {
+          *pstek = '\0';
+          if (pstek != stek) {
+              pstek--;
+          }
+      }
+      else {
+          cout << "Incorrect brackets combination" << endl;
+          return -1;
+      }
+    }
+    pstr++;
+  }
+
+  if (*pstek == '\0' && pstek == stek) {
+      cout << "Correct" << endl;
+      return 1;
+  }
+  else {
+           cout << "Incorrect brackets combination" << endl;
+          return -1;
+  }
 
   return 0;
+}
+
+int brackets(const char c) {
+  switch (c) {
+  case '(':
+    return 1;
+  case ')':
+    return -1;
+  case '[':
+    return 2;
+  case ']':
+    return -2;
+  case '{':
+    return 3;
+  case '}':
+    return -3;
+  default:
+    return 0;
+  }
 }
